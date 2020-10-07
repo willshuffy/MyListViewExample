435bbb26278a47df47d008286c9f6bf1996d56d3
willshuffy <saputrawilly26@gmail.com>
2020-10-08 03:05:59 +0700

    Simple ListView

--- app/src/main/java/com/willshuffyproject/mylistviewexample/MainActivity.kt
+++ app/src/main/java/com/willshuffyproject/mylistviewexample/MainActivity.kt
@@ -2,11 +2,21 @@ package com.willshuffyproject.mylistviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    private val dataName = arrayOf("Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Patimura", "RA Kartini", "Soekarno")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView:ListView = findViewById(R.id.lv_list)
+        val adapter = ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, android.R.id.text1, dataName)
+
+        listView.adapter = adapter
     }
 }
